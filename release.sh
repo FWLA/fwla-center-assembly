73e9b4981774d837fc8dc58ca33e1bff597e66e9
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2019-01-07 17:34:16 +0100

    First version of release script

---
+++ release.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+relVers=$1
+nextVers=$2
+
+/bin/bash ./update-version.sh $relVers
+
+cd fwla-center
+./mvnw clean install
+docker build -t ihrigb/fwla-center:latest -t ihrigb/fwla-center:$relVers --build-arg JAR_FILE=target/fwla-center-$relVers.jar .
+cd ..
+
+cd fwla-center-admin
+npm install
+npm run build
+docker build -t ihrigb/fwla-center-admin:latest -t ihrigb/fwla-center-admin:$relVers .
+cd ..
+
+cd fwla-center-display
+npm install
+ng build --prod --base-href=/display/
+docker build -t ihrigb/fwla-center-display:latest -t ihrigb/fwla-center-display:$relVers .
+cd ..
+
+if [ -f .githubapikey ]; then
+    API_KEY="`cat .githubapikey`"
+    API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Release of version %s","draft": false,"prerelease": true}' $relVers $relVers $relVers)
+    curl --data "$API_JSON" https://api.github.com/repos/FWLA/fwla-center/releases?access_token=$API_KEY
+    curl --data "$API_JSON" https://api.github.com/repos/FWLA/fwla-center-admin/releases?access_token=$API_KEY
+    curl --data "$API_JSON" https://api.github.com/repos/FWLA/fwla-center-display/releases?access_token=$API_KEY
+fi
+
+docker push ihrigb/fwla-center:latest
+docker push ihrigb/fwla-center-admin:latest
+docker push ihrigb/fwla-center-display:latest
+docker push ihrigb/fwla-center:$relVers
+docker push ihrigb/fwla-center-admin:$relVers
+docker push ihrigb/fwla-center-display:$relVers
+
+/bin/bash ./update-version.sh $nextVers
--- update-version.sh
+++ update-version.sh
@@ -2,26 +2,23 @@
 
 VERS=$1
 
+git submodule foreach git pull
+
 # Update fwla-center
 cd fwla-center
 ./mvnw versions:set -DnewVersion=$VERS -DgenerateBackupPoms=false
-git add -A
-git commit -m "Update version to $VERS"
-git push
 cd ..
 
 # Update fwla-center-admin
 cd fwla-center-admin
-jq '.version = $newVers' --arg newVers $VERS package.json > package.tmp.json && mv package.tmp.json package.json && rm package.tmp.json
-git add -A
-git commit -m "Update version to $VERS"
-git push
+jq '.version = $newVers' --arg newVers $VERS package.json > package.tmp.json && mv package.tmp.json package.json
 cd ..
 
 # Update fwla-center-display
 cd fwla-center-display
-jq '.version = $newVers' --arg newVers $VERS package.json > package.tmp.json && mv package.tmp.json package.json && rm package.tmp.json
-git add -A
-git commit -m "Update version to $VERS"
-git push
+jq '.version = $newVers' --arg newVers $VERS package.json > package.tmp.json && mv package.tmp.json package.json
 cd ..
+
+git submodule foreach git add -A
+git submodule foreach git commit -m "Update version to $VERS"
+git submodule foreach git push
