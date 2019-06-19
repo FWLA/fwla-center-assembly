d3e6fe120dcb2bb8d77495e51c90c24a1071220a
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2019-06-19 10:14:01 +0200

    Small fixes and adoptions

--- restore.sh
+++ restore.sh
@@ -8,7 +8,7 @@ source ./readenv.sh
 if [ -f $1 ]
 then
     echo "Restoring $1"
-    cat $1 | sudo docker exec -i fwla-center-postgres psql -U $POSTGRES_USER
+    cat $1 | sudo docker exec -i fwla-center-postgres psql -U $POSTGRES_USERNAME
 else
     echo "File $1 does not exist."
 fi
